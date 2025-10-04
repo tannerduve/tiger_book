aacb80dfecbaf7dd0a4604371e381b1c32c0dd52
Tanner Duve <tannerduve@gmail.com>
2025-10-04 15:07:09 -0700

    lean symbol files

--- TigerBook/Types.lean
+++ TigerBook/Types.lean
@@ -2,7 +2,16 @@ import TigerBook.Symbol
 
 open Symbol
 
-abbrev unique := IO.Ref Unit
+structure Unique where
+  id : Nat
+deriving BEq, Hashable, Ord, Repr
+
+abbrev Fresh := StateM Nat
+
+def fresh : Fresh Unique := do
+  let n ← get
+  set (n + 1)
+  pure ⟨n⟩
 
 inductive ty : Type where
 | Record : (List (Symbol × ty)) → Unique → ty
@@ -10,3 +19,11 @@ inductive ty : Type where
 | Int : ty
 | String : ty
 | Array : ty → Unique → ty
+
+def mkArray (elt : ty) : Fresh ty := do
+  let u ← fresh
+  pure (ty.Array elt u)
+
+def mkRecord (fields : List (Symbol × ty)) : Fresh ty := do
+  let u ← fresh
+  pure (ty.Record fields u)
