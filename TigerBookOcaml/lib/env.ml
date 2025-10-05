36f70da23db1e571730016e883356d137cf4ff7b
Tanner Duve <tannerduve@gmail.com>
2025-10-05 14:20:10 -0700

    remove comments

--- TigerBookOcaml/lib/env.ml
+++ TigerBookOcaml/lib/env.ml
@@ -1,14 +1,3 @@
-(* 
-signature ENV =
-sig
-type access
-type ty
-datatype enventry = VarEntry of (ty: ty)
-I FunEntry of (formals: ty list, result: ty)
-val base_tenv : ty Symbol. t a b l e * predefined types *)
-(* val b a s e venv : enventry Symbol. table (* predefined functions *
-end *) *)
-
 module type Env = sig 
 
   type access 
