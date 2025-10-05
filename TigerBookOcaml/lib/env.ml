8c57a105b644c2c119d4a6c08d55f79a1a9bcda5
Tanner Duve <tannerduve@gmail.com>
2025-10-05 15:53:35 -0700

    progress on implementing environments

--- TigerBookOcaml/lib/env.ml
+++ TigerBookOcaml/lib/env.ml
@@ -1,9 +1,27 @@
-module type Env = sig 
+open Symbol
 
-  type access 
-  type enventry = VarEntry of Types.ty 
-                  | FunEntry of {formals : Types.ty list; result : Types.ty}
-  val base_tenv : Types.ty Symbol.table
+type ty = Types.ty
+
+type enventry = VarEntry of ty 
+                  | FunEntry of {formals : ty list; result : ty}
+let base_tenv =
+  empty 
+  |> add (symbol "int") Types.INT
+  |> add (symbol "string") Types.STRING 
+
+(* TODO: FINISH *)
+let base_venv = 
+  empty 
+  |> add (symbol "print") (FunEntry {formals = [Types.STRING]; result = Types.NIL})
+  |> add (symbol "flush") (FunEntry {formals = []; result = Types.NIL})
+  |> add (symbol "getchar") (FunEntry {formals = []; result = Types.STRING})
+  |> add (symbol "ord") (FunEntry {formals = [Types.STRING]; result = Types.INT})
+  |> add (symbol "concat") (FunEntry {formals = [Types.STRING; Types.STRING]; result = Types.STRING})
+
+module type Env = sig 
+  type ty
+  type enventry
+  val base_tenv : ty Symbol.table
   val base_venv : enventry Symbol.table
 end
 
